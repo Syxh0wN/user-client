d29ce91e94f8087f3f4b301ba97bc54ca1967e2f
Syxh0wN <leonardobennr@gmail.com>
2023-03-29 20:55:49 -0300

    feat: Create route profiler

--- src/controllers/user/show.controller.ts
+++ src/controllers/user/show.controller.ts
@@ -1,18 +1,30 @@
 import { Request, Response } from "express";
-import { getOneUserService, showUserService } from "../../services/user/show.service";
-import { validate as isValidUuid } from 'uuid';
+import {
+  getOneUserService,
+  showProfilerService,
+  showUserService,
+} from "../../services/user/show.service";
+import { validate as isValidUuid } from "uuid";
+import { IUserDecode } from "../../interfaces/user/interface";
 
 export const showUserController = async (req: Request, res: Response) => {
-    const listUser = await showUserService();
-    return res.status(200).json(listUser);
+  const listUser = await showUserService();
+  return res.status(200).json(listUser);
 };
 
 export const getOneUserController = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (isValidUuid(id) === false)
-        return res.status(400).json({ message: "UUID is not valid" });
+  if (isValidUuid(id) === false)
+    return res.status(400).json({ message: "UUID is not valid" });
 
-    const user = await getOneUserService(id);
-    return res.status(200).json(user);
-}
+  const user = await getOneUserService(id);
+  return res.status(200).json(user);
+};
+
+export const showProfilerController = async (req: Request, res: Response) => {
+  const user: IUserDecode = req.userDecode;
+
+  const profiler = await showProfilerService(user);
+  return res.status(200).json(profiler);
+};
--- src/services/user/show.service.ts
+++ src/services/user/show.service.ts
@@ -1,24 +1,43 @@
 import { instanceToPlain } from "class-transformer";
 import { AppDataSource } from "../../data-source";
+import { Client } from "../../entities/Client";
 import { User } from "../../entities/User";
 import { AppError } from "../../errors/appError";
-import { IUserRequest } from "../../interfaces/user/interface";
+import { IUserDecode, IUserRequest } from "../../interfaces/user/interface";
 
 export const showUserService = async () => {
-    const userRepository = AppDataSource.getRepository(User);
-    const user = await userRepository.find();
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.find();
 
-    const plainNewUser = instanceToPlain(user) as IUserRequest;
-    return plainNewUser;
+  const plainNewUser = instanceToPlain(user) as IUserRequest;
+  return plainNewUser;
 };
 
 export const getOneUserService = async (id: string) => {
-    const userRepository = AppDataSource.getRepository(User);
-    const user = await userRepository.findOneBy({ id });
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOneBy({ id });
 
-    if (!user)
-        throw new AppError("User not found", 404);
+  if (!user) throw new AppError("User not found", 404);
 
-    const plainNewUser = instanceToPlain(user) as IUserRequest;
-    return plainNewUser;
-}
+  const plainNewUser = instanceToPlain(user) as IUserRequest;
+  return plainNewUser;
+};
+
+export const showProfilerService = async (user: IUserDecode) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const userFind = await userRepository.findOneBy({ id: user.id });
+
+  if (!userFind) throw new AppError("User not found", 404);
+
+  const clientRepository = AppDataSource.getRepository(Client);
+  const clientFind = await clientRepository.findOneBy({ id: user.id });
+
+  //const plainNewUser = instanceToPlain(userFind) as IUserRequest;
+
+  const plainNewUser = {
+    ...(instanceToPlain(userFind) as IUserRequest),
+    client: clientFind || null,
+  };
+
+  return plainNewUser;
+};
